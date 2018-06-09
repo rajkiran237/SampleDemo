4d9b6595afc91b65dd03bfb45bf018e07ffee5f5
rajkiran237 <rajkiran237@gmail.com>
2018-06-09 19:55:16 +0530

    first commit

---
+++ SampleDemo/Service1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleDemo
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            Log("OnStart");
+        }
+
+        protected override void OnStop()
+        {
+            Log("OnStop");
+        }
+
+        private List<string> Log(string data)
+        {
+            try
+            {
+                List<string> lines = new List<string>();
+                lines.AddRange(
+                new[] {
+                        data,
+                        "In log"
+                });
+
+                File.AppendAllLines("c:\\SampleDemo.log.txt", lines);
+            }
+            catch (Exception ex)
+            {
+                lines.AddRange(
+                    new[] {
+                        "Exception:",
+                        ex.Message,
+                        ex.StackTrace
+                    });
+            }
+
+            return lines;
+        }
+    }
+}
