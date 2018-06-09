3731b175979a38ea4c97216536525d46d7dd8ecb
rajkiran237 <rajkiran237@gmail.com>
2018-06-09 21:33:49 +0530

    Added polling code again.

--- SampleDemo/Service1.cs
+++ SampleDemo/Service1.cs
@@ -36,7 +36,7 @@ namespace SampleDemo
                 lines.AddRange(
                 new[] {
                         data,
-                        "In log test for continuous integration of 5 minutes."
+                        "In log test for continuous integration of 5 minutes. Added some random text."
                 });
 
                 File.AppendAllLines("c:\\SampleDemo.log.txt", lines);
