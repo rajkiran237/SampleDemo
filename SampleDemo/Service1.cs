aa1441b156b7700db66f4b095152c6ec0e172100
rajkiran237 <rajkiran237@gmail.com>
2018-06-11 12:56:38 +0530

    Added post-receive hook .

--- SampleDemo/Service1.cs
+++ SampleDemo/Service1.cs
@@ -36,7 +36,7 @@ namespace SampleDemo
                 lines.AddRange(
                 new[] {
                         data,
-                        "In log test for continuous integration of 5 minutes. Added some random text. Added post-receive hook."
+                        "In log test for continuous integration of 5 minutes. Added some random text. Added post-receive hook ."
                 });
 
                 File.AppendAllLines("c:\\SampleDemo.log.txt", lines);
